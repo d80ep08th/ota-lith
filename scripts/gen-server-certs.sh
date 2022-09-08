5104f8de3c4d846c1ca725a76ecd9e60cedb1942
Parth Dode <dodecoder@gmail.com>
2022-09-08 14:40:07 +0530

    new sub-domain -added dgw.uptanedemo.org

--- scripts/gen-server-certs.sh
+++ scripts/gen-server-certs.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SERVER_DIR=ota-ce-gen
 DEVICES_DIR=ota-ce-gen/devices
 CWD=$(dirname $0)
-SERVER_NAME=uptanedemo.org
+SERVER_NAME=dgw.uptanedemo.org
 
 if [ -d "$SERVER_DIR" ] || [ -d "$DEVICES_DIR" ] ; then
     echo "${SERVER_DIR} or ${DEVICES_DIR} exists, aborting"
